a373e534a10c3c25af83a0e6f8e40c189388a1ff
Igor Esposito <sposigor@gmail.com>
2021-12-02 17:57:06 -0300

    ajustes

--- Nivel - Intermediario dia 15 a 58/exercicios/dia 16/inciando_poo.py
+++ Nivel - Intermediario dia 15 a 58/exercicios/dia 16/inciando_poo.py
@@ -1,16 +1,23 @@
 ''' Iniciando com orientação a objetos '''
 
-from turtle import Turtle, Screen
-import outro_modulo
-print(outro_modulo.variavel)
+# from turtle import Turtle, Screen
+# import outro_modulo
+# print(outro_modulo.variavel)
+# 
+# destroy = Turtle()
+# print(destroy)
+# destroy.shape('turtle')
+# destroy.color('red')
+# destroy.speed(1)
+# destroy.forward(100)
+# 
+# tela = Screen()
+# print(tela.canvheight)
+# tela.exitonclick()
 
-destroy = Turtle()
-print(destroy)
-destroy.shape('turtle')
-destroy.color('red')
-destroy.speed(1)
-destroy.forward(100)
+from prettytable import PrettyTable
 
-tela = Screen()
-print(tela.canvheight)
-tela.exitonclick()
+tabela = PrettyTable()
+tabela.add_column('Pokemon', ['Pikachu', 'Squirtle', 'Charmander'], align='l')
+tabela.add_column('Tipo', ['Elétrico', 'Água', 'Fogo'], align='l')
+print(tabela)
