82247bec6ccd0ce8e140da9d366d1ace4da02790
Igor Esposito <sposigor@gmail.com>
2022-01-20 11:14:23 -0300

    exercicio

--- Nivel - Intermediario dia 15 a 58/exercicios/dia 27/main.py
+++ Nivel - Intermediario dia 15 a 58/exercicios/dia 27/main.py
@@ -1,13 +1,35 @@
 ''' Exercicio do dia 27 '''
 
-import tkinter
+from tkinter import Tk, Label, Entry, Button
 
-janela = tkinter.Tk()
-janela.title('Minha primeira GUI')
-janela.minsize(width=500, height=300)
+janela = Tk()
+janela.title('Conversor de Kms para Milhas')
+janela.minsize(width=250, height=100)
+janela.config(padx=20, pady=20)
 
 
-etiqueta = tkinter.Label(janela, text='Funcionalidades:', font=('Arial', '12'))
-etiqueta.pack(expand=True, fill='both')
+def kms_para_milhas():
+    ''' Função para converter Kms para Milhas '''
+    kms = float(entrada_kms.get())
+    milhas = round(kms * 0.621371)
+    resultado_milhas.config(text=f'{milhas}')
+
+entrada_kms = Entry(width=7)
+entrada_kms.grid(row=0, column=1)
+
+kms_label = Label(text='Kms')
+kms_label.grid(row=0, column=2)
+
+igual_a = Label(text='Igual á')
+igual_a.grid(row=1, column=0)
+
+resultado_milhas = Label(text='0')
+resultado_milhas.grid(row=1, column=1)
+
+resultado_label = Label(text='Milhas')
+resultado_label.grid(row=1, column=2)
+
+botão_calcular = Button(text='Calcular', command=kms_para_milhas)
+botão_calcular.grid(row=2, column=1)
 
 janela.mainloop()
